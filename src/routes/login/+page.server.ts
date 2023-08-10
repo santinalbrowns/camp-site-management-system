c0ac5a4d2c3bc346462699290b99560260fb3448
Santinal Browns <asantikuseka@outlook.com>
2023-08-10 12:53:44 +0200

    add login

---
+++ src/routes/login/+page.server.ts
@@ -0,0 +1,47 @@
+import { prisma } from '$lib/database';
+import { fail, redirect } from '@sveltejs/kit';
+import * as bcrypt from "bcrypt";
+import type { PageServerLoad } from './$types';
+
+export const load = (async () => {
+    return {};
+}) satisfies PageServerLoad;
+
+export const actions = {
+    default: async ({ cookies, request }) => {
+        const data = await request.formData();
+
+        const email = data.get('email')?.toString();
+        const password = data.get('password')?.toString();
+
+        if (!email || !password) {
+            return fail(401, { invalid: true });
+        }
+
+        const user = await prisma.user.findUnique({
+            where: { email: email }
+        })
+
+        if (!user) {
+            return fail(400, { credentials: true })
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return fail(400, { credentials: true })
+        }
+
+        cookies.set('session', user.id, {
+            path: '/',
+            httpOnly: true,
+            sameSite: 'strict',
+            maxAge: 60 * 60 * 24 * 30, // set cookie to expire after a month
+        });
+
+        console.log("All Good");
+
+        // redirect the user
+        //throw redirect(302, '/')
+    }
+}
