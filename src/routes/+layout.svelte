c484a444d78081bee0ddf26135b8a8803d063e48
Santinal Browns <asantikuseka@outlook.com>
2023-08-11 10:30:52 +0200

    update dashboard

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
+	import 'material-symbols';
 </script>
 
 <slot />
--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,1 +1,10 @@
+<aside class="p-6">
+    <nav>
+        <a href="/admin">
+            <span>Dashboard</span>
+        </a>
+    </nav>
+</aside>
+
 <slot/>
+
