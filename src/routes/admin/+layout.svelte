482bc87039cbebc5b01652c3e6011d64f42310cc
Santinal Browns <asantikuseka@outlook.com>
2023-08-11 11:51:20 +0200

    add auth

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -4,6 +4,10 @@
             <span>Dashboard</span>
         </a>
     </nav>
+
+    <form action="/logout" method="post" class="py-2">
+        <button type="submit">Logout</button>
+    </form>
 </aside>
 
 <slot/>
